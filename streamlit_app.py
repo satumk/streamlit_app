bba7456ae852be15dad362fd38af6bd879a54e64
Satu <46418840+satumk@users.noreply.github.com>
2022-11-04 14:08:12 +0200

    Update streamlit_app.py
    add another user input box

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # outputs a screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+#allow user to add
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
