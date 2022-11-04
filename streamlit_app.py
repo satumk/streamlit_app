8caf75d19084253b11f8d357236a11113db4a3dc
Satu <46418840+satumk@users.noreply.github.com>
2022-11-04 14:10:42 +0200

    Update streamlit_app.py
    add to snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,13 +30,15 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # outputs a screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-#allow user to add
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#allow user to add
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
