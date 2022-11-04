610e8b714628514fa45611a8d92da472d71324e4
Satu <46418840+satumk@users.noreply.github.com>
2022-11-04 12:26:48 +0200

    Update streamlit_app.py
    towards variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
 # takes json and normalizes the response 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # outputs a screen as a table
