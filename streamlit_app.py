d8fa08471bc0a9aee301b88a102cd2802f07b13e
Satu <46418840+satumk@users.noreply.github.com>
2022-11-04 14:32:55 +0200

    Update streamlit_app.py
    try-except for fruity vice

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,15 +21,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else :
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # takes json and normalizes the response 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # outputs a screen as a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# takes json and normalizes the response 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# outputs a screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
